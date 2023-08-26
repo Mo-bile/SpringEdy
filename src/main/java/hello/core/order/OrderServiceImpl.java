b4a379f55df9faaecbe23eaee4a59fefb6f8d884
MoJaeYoung <jym2013@gmail.com>
2023-08-26 22:34:58 +0900

    feat : section2

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,28 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+
+    /*
+    * 이게 설계가 잘 된거임
+    * orderservice 입장에서는 몰라 discountPolicy 니가 알아서해
+    * 결과만 나한테 알려줘 -> 단일체계원칙 준수함
+    * */
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
